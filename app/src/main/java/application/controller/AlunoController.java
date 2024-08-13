0842035e954240039072aa8a4446c3a7e29619b0
Marcos Hideki <jorge.sakaguchi@outlook.com>
2024-08-13 19:51:49 +0000

    refactor: alter mode

--- app/src/main/java/application/controller/AlunoController.java
+++ app/src/main/java/application/controller/AlunoController.java
@@ -4,23 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import application.model.Aluno;
 import application.repository.AlunoRepository;
 
 @RestController
+@RequestMapping("/alunos")
 public class AlunoController {
     @Autowired
     private AlunoRepository alunoRepository;
 
-    @GetMapping("/alunos")
+    @GetMapping
     public Iterable<Aluno> list(){
        return  alunoRepository.findAll();
  
     }
 
-    @PostMapping("/alunos")
+    @PostMapping
     public Aluno insert(@RequestBody Aluno aluno){
         return alunoRepository.save(aluno);
     }
